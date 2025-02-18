b46e2b795c4b5085c709950ad08b3587e8b9123d
JokerHin <chokarhin@gmail.com>
2025-02-18 22:16:34 +0800

    update deploy

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -33,8 +33,8 @@ export const register = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "Lax" : "Strict",
+      secure: true, // Ensure this is true
+      sameSite: "None", // Change to None
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -83,8 +83,8 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "Lax" : "Strict",
+      secure: true, // Ensure this is true
+      sameSite: "None", // Change to None
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -98,8 +98,8 @@ export const logout = async (req, res) => {
   try {
     res.clearCookie("token", {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "Lax" : "Strict",
+      secure: true, // Ensure this is true
+      sameSite: "None", // Change to None
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
