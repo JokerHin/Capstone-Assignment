bd1c18fa6bf1b0e08540bff0041f3b9a6dae12b7
Yong Wai <yongwaichong@gmail.com>
2025-05-08 01:13:48 +0800

    Created updateUserLocation controller

--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getUserData, updateProfile } from "../controllers/userController.js";
+import { getUserData, updateProfile, updateUserLocation } from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/data", getUserData);
 userRouter.put("/update-profile", updateProfile);
+userRouter.post("/update-location", updateUserLocation);
 
 export default userRouter;
